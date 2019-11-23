d083829500c9a50a0a933834d3715d42de4fb954
Ragusaen <rtollu18@student.aau.dk>
2019-11-23 11:54:15 +0100

    pls

---
+++ CreateJam2019/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Throw()
+    {
+
+    }
+}
